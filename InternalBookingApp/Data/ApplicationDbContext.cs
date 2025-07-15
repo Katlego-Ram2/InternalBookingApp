84e2a12fe5e0fc0d76e9fcb8ac1ab0caf18ff5d9
katlego-ram2 <rampedikatleho@gmail.com>
2025-07-15 11:36:31 +0200

    add:added resource management

--- InternalBookingApp/Data/ApplicationDbContext.cs
+++ InternalBookingApp/Data/ApplicationDbContext.cs
@@ -1,1 +1,24 @@
-// ApplicationDbContext.cs placeholder content
+using Microsoft.EntityFrameworkCore;
+using InternalBookingApp.Models;
+
+namespace InternalBookingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Resource>()
+                .HasMany(r => r.Bookings)
+                .WithOne(b => b.Resource)
+                .HasForeignKey(b => b.ResourceId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
